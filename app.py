ec0f2c890a3e9c55948a1676e0a50293ff3705e8
Viktor <viktor.tsvil@gmail.com>
2024-07-27 13:33:30 -0700

    init flask commit

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from pprint import pprint
 
 from flask import Flask
 
-from server.api import get_scores
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def hello_world():
-    result = get_scores()
-    pprint(result)
-    return f"<p>{result}</p>"
+    return f"<p>hello world</p>"
 
 
 if __name__ == "__main__":
