ffa8919e42e5aee3526214f63b0c49114067ce94
hugosiuyh <93142129+hugosiuyh@users.noreply.github.com>
2024-07-27 13:54:25 -0700

    index.html

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, jsonify, request
 from server.utils import generate_notifications
 
 app = Flask(__name__)
 
-
 @app.route("/notifications/", methods=["GET"])
-def hello_world():
+def notifications():
     notifs = generate_notifications(None)
-    return render_template(notifs)
+    return render_template('index.html', notifs=notifs)
 
+@app.route('/create_suggestion', methods=['POST'])
+def create_suggestion():
+    # Perform the action you want when the notification is clicked
+    print("Notification clicked!")
+    js = request.json
+    print(js)
+    return jsonify({"message": "Notification handled"})
 
 if __name__ == "__main__":
     app.run(debug=True)
